3d942dc402afbe6a7d211310a2ef69c4c2ba56c4
Ricardo Gomes <desk467@gmail.com>
2015-07-08 12:01:39 -0300

    fixed issue on meteor.lua (it was upside down)

--- meteor.lua
+++ meteor.lua
@@ -9,14 +9,17 @@ function Meteor:init(x, y, life)
   self.lookAt   = math.atan2(160- self.y,  240- self.x)
   self.velocity = 65
   self.scale = 1
-  self.life = life
-  
+  self.life  = life
+  self.where = self.x  
   self.img  = love.graphics.newImage('/res/img/meteor_anim.png')
   self.g    = anim8.newGrid(64, 53, self.img:getWidth(), self.img:getHeight())
   self.meteor_anim = anim8.newAnimation(self.g('1-5',1), {2, 0.1,0.5,0.1,0.1})
   
-  self.size = 64*self.scale
+  if self.where > 0 then
+    self.meteor_anim = self.meteor_anim:flipV()
+  end
   
+  self.size = 64*self.scale
 end
 
 function Meteor:draw()
